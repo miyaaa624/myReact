705abac988a558706e5b9563dc469b6c2059c204
miyaaa624 <a09837290595@gmail.com>
2024-11-20 11:22:33 +0800

    1130

--- my-react-todo/react-todo/src/components/EditForm.jsx
+++ my-react-todo/react-todo/src/components/EditForm.jsx
@@ -1,27 +1,21 @@
 import { useState } from "react"
-
-//輸入表單的元件 (加入的那部分)
-function CreateForm({ addTodo }) {
-
+function EditForm({todo,editTodo}) {
     // 建立input內容的變數
     // const[變數名稱,更改變數的方法]=useState(初始值);
-    const [content, setContent] = useState('');
+    //useState()初始值不是空字串,設定為修改中的todo內容
+    const [content, setContent] = useState(todo.content);
 
     //按下按鈕增加內容
     const handleSubmit = (e) => {
         //取消事件預設行為
         e.preventDefault();
-        //增加todo內容
-        addTodo(content);
-        //清除input內容 (文字框內容)
-        setContent('');
+        editTodo(todo.id,content)
     }
 
     return (
         // 建立表單
         <form className="create-form" onSubmit={handleSubmit}>
-            <input type="text" placeholder="輸入待辦事項"
-
+            <input type="text"
                 // seState 通常需要搭配 value 和 onChange，特別是在處理表單輸入時。 
                 // 這是因為在 React 中，表單輸入框通常設為「受控元件」，以便 React 完全掌控其值。
                 // value一定要搭配onChange 
@@ -33,9 +27,8 @@ function CreateForm({ addTodo }) {
                     setContent(e.target.value)
                 }}
             />
-            <button type="submit">加入</button>
+            <button type="submit">完成</button>
         </form>
     )
 }
-
-export default CreateForm 
+export default EditForm
