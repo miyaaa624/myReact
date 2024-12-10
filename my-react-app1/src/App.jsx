e5f6f57a980eeffd7b3e26a27fbada16ea5b32fe
miyaaa624 <a09837290595@gmail.com>
2024-12-10 15:54:35 +0800

    1210-6

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,13 +1,36 @@
 import $ from 'jquery'
+import { useEffect } from 'react';
 // import jquery
 // 不用寫$(function(){})
 export default function App() {
+
+
+ //寫法1 : 過時寫法
+//   useEffect(() => {
+//     $('.cssAnim1').hover(function(){
+//       $(this).addClass('.cssAnim1:hover');
+//   },function(){
+//       $(this).removeClass('imgScale');
+//   })
+// }, [])
+
+  //寫法2 : 建議寫法
+  useEffect(() => {
+    $('.cssAnim1').on('mouseover', function () {
+      $(this).addClass('imgScale');
+    })
+    $('.cssAnim1').on('mouseout', function () {
+      $(this).removeClass('imgScale');
+    })
+  }, [])
+
   return (
     <div>
-      <h2>jquery</h2>
-      <button onClick={()=>{
-        console.log($('h2').text())
-      }}>click</button>
+      <a href="#" class="cssAnim1">
+        {/* 結尾標籤 */}
+        {/* 圖片要在public才抓的到 */}
+        <img src="./images/03.jpg" alt="" />
+      </a>
     </div>
   )
 }
