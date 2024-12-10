3a057beffd3106163c4be58d607e6b6eaf766d59
miyaaa624 <a09837290595@gmail.com>
2024-12-10 09:50:49 +0800

    1210-2

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,10 +1,13 @@
 import $ from 'jquery'
-
+// import jquery
+// 不用寫$(function(){})
 export default function App() {
   return (
-   <div>
-    <h2>jquery</h2>
-    <button>click</button>
-   </div>
+    <div>
+      <h2>jquery</h2>
+      <button onClick={()=>{
+        console.log($('h2').text())
+      }}>click</button>
+    </div>
   )
 }
